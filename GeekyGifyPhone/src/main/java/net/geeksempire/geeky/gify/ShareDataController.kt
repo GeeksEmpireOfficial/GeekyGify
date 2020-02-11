21cd73be0396f04556dc650b4bc48e824b8975b3
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-02-11 12:27:31 -0900

    [Time] 11-Feb-2020 | 12:25
    [Description]
    Phone Module Controller Added
    
    [Phone][ChangeLog]
    Phone Module to Control Sharing Data & Opening Sharing Menu
    
    [Watch][ChangeLog]
    Sharing Option Action Completed {
    ~ Share to Other Applications on Watch
    ~ Open Sharing Menu on Phone
    }
    
    [Build][ChangeLog]
    Adding Initial Dependencies

--- GeekyGifyPhone/src/main/java/net/geeksempire/geeky/gify/ShareDataController.kt
+++ GeekyGifyPhone/src/main/java/net/geeksempire/geeky/gify/ShareDataController.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire. 
  *
- * Created by Elias Fazel on 2/11/20 12:26 PM
- * Last modified 2/11/20 12:26 PM
+ * Created by Elias Fazel on 2/11/20 12:27 PM
+ * Last modified 2/11/20 12:27 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT

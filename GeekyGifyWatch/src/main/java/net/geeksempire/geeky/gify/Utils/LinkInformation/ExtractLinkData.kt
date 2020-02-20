5e93c0381f3c91163756563fa8b96968723cf2bc
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-02-19 17:40:50 -0900

    [Time] 19-Feb-2020 | 17:35
    [Description]
    
    [Watch][ChangeLog]
    3. Save GIF File with Unique Id to Avoid Duplication
    2. Optimized Process of Category List Items Populating
    1. Round/Rectangle/Chin Display Support Optimization Added
    
    [Phone][ChangeLog]
    2. Save GIF File with Unique Id to Avoid Duplication
    1. Separated Process Of Received Data to Show Information on Null Data
    
    [Build][ChangeLog]
    Adding Initial Dependencies

--- GeekyGifyWatch/src/main/java/net/geeksempire/geeky/gify/Utils/LinkInformation/ExtractLinkData.kt
+++ GeekyGifyWatch/src/main/java/net/geeksempire/geeky/gify/Utils/LinkInformation/ExtractLinkData.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 2/19/20 5:35 PM
- * Last modified 2/19/20 5:26 PM
+ * Created by Elias Fazel on 2/19/20 5:40 PM
+ * Last modified 2/19/20 5:35 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT

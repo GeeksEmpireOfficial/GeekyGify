5dae58d0b02ff05b6478567f295a2d9f4cbb773f
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-03-03 02:00:39 -0900

    [Time] 03-Mar-2020 | 02:00
    [Description]
    
    [Watch][ChangeLog]
    
    [Phone][ChangeLog]
    1. Initializing Trending and Collection Section
    
    [Build][ChangeLog]
    Adding Initial Dependencies

--- GeekyGifyPhone/src/main/java/net/geeksempire/geeky/gify/ServerConnection/DownloadData/GiphyJsonDataStructure.kt
+++ GeekyGifyPhone/src/main/java/net/geeksempire/geeky/gify/ServerConnection/DownloadData/GiphyJsonDataStructure.kt
@@ -2,7 +2,7 @@
  * Copyright © 2020 By Geeks Empire.
  *
  * Created by Elias Fazel on 3/3/20 2:00 AM
- * Last modified 2/11/20 11:17 AM
+ * Last modified 3/3/20 2:00 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT

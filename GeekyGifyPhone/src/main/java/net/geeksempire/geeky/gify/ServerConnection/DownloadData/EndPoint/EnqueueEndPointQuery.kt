17051e2f12f5a8b5ca9feaeed0047349f4ed925e
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-03-05 08:08:14 -0900

    [Time] 05-Mar-2020 | 08:00
    [Description]
    
    [Watch][ChangeLog]
    
    [Phone][ChangeLog]
    2. Giphy Explore Section Added
    1. UI Optimization for Collection Section
    
    [Build][ChangeLog]
    Adding Initial Dependencies

--- GeekyGifyPhone/src/main/java/net/geeksempire/geeky/gify/ServerConnection/DownloadData/EndPoint/EnqueueEndPointQuery.kt
+++ GeekyGifyPhone/src/main/java/net/geeksempire/geeky/gify/ServerConnection/DownloadData/EndPoint/EnqueueEndPointQuery.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 3/5/20 8:01 AM
- * Last modified 3/5/20 6:53 AM
+ * Created by Elias Fazel on 3/5/20 8:08 AM
+ * Last modified 3/5/20 8:01 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
--- GeekyGifyPhone/src/main/java/net/geeksempire/geeky/gify/ServerConnection/DownloadData/JsonData/GiphyJsonDataStructure.kt
+++ GeekyGifyPhone/src/main/java/net/geeksempire/geeky/gify/ServerConnection/DownloadData/JsonData/GiphyJsonDataStructure.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 3/5/20 8:01 AM
- * Last modified 3/3/20 2:00 AM
+ * Created by Elias Fazel on 3/5/20 8:08 AM
+ * Last modified 3/5/20 8:01 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
